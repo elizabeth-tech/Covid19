99bb3e6c25e510d60f493bfa7d894aa05fdb74f9
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-08 16:40:03 +0700

    Команда закрытия главного окна в модели-представления

--- Covid19/ViewModels/MainWindowViewModel.cs
+++ Covid19/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Covid19.ViewModels.Base;
+﻿using Covid19.Infrastructure.Commands;
+using Covid19.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Covid19.ViewModels
 {
     class MainWindowViewModel : ViewModel
     {
+        public MainWindowViewModel()
+        {
+            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+        }
+
+
         #region Свойства
 
         #region Заголовок окна
@@ -42,7 +51,26 @@ namespace Covid19.ViewModels
 
         #endregion
 
+        #region Команды
 
+        #region CloseApplicationCommand
+        // Сама команда
+        public ICommand CloseApplicationCommand { get; }
+
+        // Доступна ли команда для выполнения
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        // Свойства, определяющие команду
+        // В момент выполнения
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+       
+        #endregion
+
+        #endregion
 
     }
 }
