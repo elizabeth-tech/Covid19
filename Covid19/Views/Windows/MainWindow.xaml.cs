63adfa3445383eefbd3f3cafeb9c6ccb68720744
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-08 15:39:53 +0700

    Создание базовой инфраструктуры приложения

---
+++ Covid19/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Covid19
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
