689a06189b1fffe72b0871d065f55e33d9f1cfdd
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-08 15:52:09 +0700

    Создана базовая модель-представление. Начата работа над моделью-представлением главного окна

---
+++ Covid19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Covid19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Свойства
+
+        #region Заголовок окна
+        private string title = "Анализ статистики Covid-19";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => title;
+            // Вот этот кусок кода:
+            //set
+            //{
+            //    if (Equals(title, value)) return;
+            //    title = value;
+            //    OnPropertyChanged();
+            //}
+            // Можно заменить на:
+            set => Set(ref title, value);
+        }
+        #endregion
+
+        #endregion
+
+
+
+    }
+}
