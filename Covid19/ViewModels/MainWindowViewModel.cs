150fe94e53ed0547ddc0543dcc490de3324a9c26
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-09 17:51:25 +0700

    На график выведены тестовые данные. Отображена кривая

--- Covid19/ViewModels/MainWindowViewModel.cs
+++ Covid19/ViewModels/MainWindowViewModel.cs
@@ -16,7 +16,7 @@ namespace Covid19.ViewModels
 
             var data_points = new List<DataPoint>((int)(360 / 0.1));
 
-            for (var x = 0d; x <= 360; x += 1)
+            for (var x = 0d; x <= 360; x += 0.1)
             {
                 const double to_rad = Math.PI / 180;
                 var y = Math.Sin(x * to_rad);
