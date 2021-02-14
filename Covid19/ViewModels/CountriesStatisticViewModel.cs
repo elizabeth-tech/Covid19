eb7e2c07b8246b273e17428ab7eccaf0b7513865
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-14 15:44:50 +0700

    Базовая логика модели-представления статистики

--- Covid19/ViewModels/CountriesStatisticViewModel.cs
+++ Covid19/ViewModels/CountriesStatisticViewModel.cs
@@ -1,21 +1,55 @@
-﻿using Covid19.Services;
+﻿using Covid19.Infrastructure.Commands;
+using Covid19.Models;
+using Covid19.Services;
 using Covid19.ViewModels.Base;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Windows.Input;
 
 namespace Covid19.ViewModels
 {
     internal class CountriesStatisticViewModel : ViewModel
     {
+        #region Свойства
+
         private DataService dataService; // сервис, который извлекает данные
 
-        private  MainWindowViewModel mainModel { get; }
+        private MainWindowViewModel mainModel { get; }
+
+        #region Информация по странам
+
+        /// <summary>Статистика по странам</summary>
+        private IEnumerable<CountryInfo> countries;
+
+        /// <summary>Статистика по странам</summary>
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => countries;
+            private set => Set(ref countries, value);
+        }
+
+        #endregion
+
+        #endregion
 
         public CountriesStatisticViewModel(MainWindowViewModel mainModel)
         {
             this.mainModel = mainModel;
             dataService = new DataService();
+
+            // Получаем данные по странам
+            RefreshDataCommand = new ActionCommand(OnRefreshDataCommandExecuted);
         }
+
+        #region Команды
+
+        #region RefreshDataCommand
+
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecuted(object p) => Countries = dataService.GetData();
+
+        #endregion
+
+        #endregion
     }
 }
