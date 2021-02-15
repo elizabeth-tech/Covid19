f3da4c92c2bee658109ad8403b902d4053438601
elizabeth-tech <elizabeth-tech@gmail.com>
2021-02-15 23:25:51 +0700

    Добавление карты. Реализация конвертера координат

---
+++ Covid19/Infrastructure/Converters/PointToMapLocation.cs
@@ -0,0 +1,24 @@
+﻿using MapControl;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Covid19.Infrastructure.Converters
+{
+    internal class PointToMapLocation : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Point point)) return null;
+            return new Location(point.X, point.Y);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Location location)) return null;
+            return new Point(location.Latitude, location.Longitude);
+        }
+    }
+}
